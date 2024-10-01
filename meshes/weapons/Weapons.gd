d81517d2bebce9ef855544b870cb89aa02d334fd
wabbaskett <36274475+wabbaskett@users.noreply.github.com>
2024-10-01 01:10:13 -0500

    Merge branch 'player' into ui-&-menus

---
+++ meshes/weapons/Weapons.gd
@@ -0,0 +1,8 @@
+class_name Weapons extends Resource
+
+@export var name : StringName
+@export_category("Weapon Orientation")
+@export var position : Vector3
+@export var rotation : Vector3
+@export_category("Weapon Visuals")
+@export var mesh : Mesh
