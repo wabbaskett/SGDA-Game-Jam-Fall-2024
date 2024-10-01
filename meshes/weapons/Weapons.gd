b0e0b5f4b38f74675bc08e04b427815dfd8bb045
wabbaskett <wesbaskett@gmail.com>
2024-10-01 01:12:51 -0500

    Revert "Merge branch 'player' into ui-&-menus"
    This reverts commit 5bc53b4cbc49968c152e60813d1a3668c4922432, reversing
    changes made to 7318f31b97ab874645c7fd5ba781fbf6551f0e36.

--- meshes/weapons/Weapons.gd
+++
@@ -1,8 +0,0 @@
-class_name Weapons extends Resource
-
-@export var name : StringName
-@export_category("Weapon Orientation")
-@export var position : Vector3
-@export var rotation : Vector3
-@export_category("Weapon Visuals")
-@export var mesh : Mesh
