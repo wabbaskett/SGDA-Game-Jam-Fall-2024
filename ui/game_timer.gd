a36a783c14f8ea0464683f2837bbbdebe8a4d954
Emilio Ortiz <96208007+PolyNoodle94@users.noreply.github.com>
2024-10-05 20:18:48 -0500

    Got loss and win messages to work
    No details

---
+++ ui/game_timer.gd
@@ -0,0 +1,18 @@
+extends Timer
+
+signal lost
+
+@onready var check_win = $"../../../Collectibles/TextEdit"
+
+func _ready() :
+	if check_win:
+		check_win.won.connect(_on_win)
+
+func _on_win() :
+	self.paused = true
+	$"../../../WinLabel".visible = true
+
+func _on_timeout() -> void:
+	emit_signal("lost")
+	$"../1SecTimer".paused = true
+	$"../../../LoseLabel".visible = true
--- ui/text_edit.gd
+++ ui/text_edit.gd
@@ -5,12 +5,15 @@ extends TextEdit
 @export var total : int = 10
 @export var num_collected : int = 0
 
+signal won
+
 func _ready() :
-	print(get_parent().get_parent().get_parent().get_parent().get_parent().get_node("./Collectibles"))
 	if (collectibles_node) :
 		collectibles_node.collected2.connect(_on_collection)
-	
+
 
 func _on_collection() : 
 	num_collected += 1
 	self.text = "Collectibles: " + str(num_collected) + " / " + str(total)
+	if num_collected == 10 :
+		emit_signal("won")
